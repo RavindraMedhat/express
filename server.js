10a5e990773a1fc57edcf7a60e7bc9a3679601c4
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-08-08 01:23:29 +0530

    add sister info

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express();
 
 app.set("view engine", "ejs")
+// app.set('views', __dirname + '/views');
 
 // app.set("views","")
+
+const sistersData = [
+  { name: 'Rajvi', study: 'Computer Science', socialMedia: 'https://example.com/rajvi' },
+  { name: 'Sister2', study: 'Art History', socialMedia: 'https://example.com/sister2' },
+];
+
+app.get('/', (req, res) => {
+  const name = req.query.name || 'Guest';
+  const sister = sistersData.find((sister) => sister.name === name);
+  res.render('index', { name, sister });
+});
 
 app.get("/", (req, res) => {
     res.render("page1", {
@@ -26,3 +38,4 @@ app.get("/", (req, res) => {
 app.listen(7485, () => {
     console.log("server is listing on 7485");
 })
+
