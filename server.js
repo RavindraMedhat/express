6d69ee7574c50bb8d10098175591e5c62d2b1eed
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-08-11 15:01:41 +0530

    change html to ejs

--- server.js
+++ server.js
@@ -4,9 +4,26 @@ const app = express();
 app.set("view engine", "ejs")
 app.use(express.static("views"));
 
+const profileData = {
+    // Provide the dynamic data for the profile here
+    name: 'Rajvi',
+    photo: 'Rajvi.jpg',
+    about: 'Hello! I\'m Rajvi...',
+    education: 'I graduated with a Bachelor\'s degree...',
+    jobTitle: 'Marketing Manager',
+    company: 'ABC Company',
+    workExperience: 'I\'m currently working as a Marketing Manager...',
+    email: 'email@example.com',
+    linkedin: 'https://www.linkedin.com/[your-profile]',
+    instagram: 'https://www.instagram.com/[your-profile]',
+    facebook: 'https://www.facebook.com/[your-profile]',
+    phoneNumber: '9624707485',
+    whatsapp: 'https://wa.me/9624707485',
+    year: '2023'
+};
 
 app.get('/', (req, res) => {
-    res.render('info');
+    res.render('info', { profile: profileData });
 });
 
 app.get("/page1", (req, res) => {
